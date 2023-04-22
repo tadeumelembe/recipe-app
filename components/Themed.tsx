dad59e569fe4f92f29e0145b7a8005ee597ea6a4
tadeumelembe <tadeumelembe@gmail.com>
2023-04-22 02:33:18 +0200

    Added Default Flatlist

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -9,14 +9,15 @@ import {
   ScrollView as DefaultScrollView,
   ImageBackground as DefaultImageBackground,
   TextInput as DefaultTextInput,
-  TouchableOpacity as DefaultTouchableOpacity
+  TouchableOpacity as DefaultTouchableOpacity,
+  FlatList as DefaultFlatList
 } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import styles from '../constants/style';
+import useGlobalStyles from '../constants/style';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export function useThemeColor(
@@ -43,47 +44,111 @@ export type ViewProps = ThemeProps & DefaultView['props'];
 export type ScrollViewProps = ThemeProps & DefaultScrollView['props'];
 export type ImageBackgroundProps = ThemeProps & DefaultImageBackground['props'];
 export type TextInputProps = ThemeProps & DefaultTextInput['props'];
+export type FlatListProps = ThemeProps & DefaultFlatList['props'];
 export type TouchableOpacityProps = ThemeProps & DefaultTouchableOpacity['props'] & {
   btnText: string
 };
+export type AvatarProps = ViewProps & { size?: string };
 
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const color = 'light';//useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = 'light';// useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
 
+export function FlatList(props: FlatListProps) {
+  const { ...otherProps } = props;
+  return (
+    <DefaultFlatList
+      style={{ width: '100%' }}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+      {...otherProps}
+    />
+  )
+}
+
+export function Avatar(props: AvatarProps) {
+  const styles = useGlobalStyles()
+
+  const { style, size, ...otherProps } = props;
+  let width = 30
+  let height = 30
+
+  switch (size) {
+    case 'xxs':
+      width = 20
+      height = 20
+      break;
+    case 'xs':
+      width = 32
+      height = 32
+      break;
+    case 'sm':
+      width = 42
+      height = 42
+      break;
+    case 'md':
+      width = 55
+      height = 55
+      break;
+
+    case 'lg':
+      width = 70
+      height = 70
+      break;
+
+    case 'xl':
+      width = 82
+      height = 82
+      break;
+
+    default:
+      break;
+  }
+
+  return <DefaultView
+    style={[{ ...styles.avatar, width: width, height: height }, style]}
+    {...otherProps}
+  />
+}
+
 export function Container(props: ViewProps) {
+  const styles = useGlobalStyles()
+
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
   const insets = useSafeAreaInsets();
 
-  return <DefaultView style={[{ backgroundColor, paddingTop: insets.top }, style, styles.container]} {...otherProps} />;
+  return <DefaultView style={[styles.container, { backgroundColor, paddingTop: insets.top }, style,]} {...otherProps} />;
 }
 
 export function ScrollView(props: ScrollViewProps) {
   const { style, ...otherProps } = props
+  const styles = useGlobalStyles()
 
   return <DefaultScrollView showsVerticalScrollIndicator={false} style={[styles.scrollContainer, style]} {...otherProps} />;
 }
 
 export function ImageBackground(props: ImageBackgroundProps) {
   const { style, ...otherProps } = props
+  const styles = useGlobalStyles()
 
   return <DefaultImageBackground style={[styles.backgorundImage, style]} {...otherProps} />;
 }
 
 export function TextInput(props: TextInputProps) {
   const { placeholder, style, ...otherProps } = props
-  
+  const styles = useGlobalStyles()
+
   return (
     <DefaultView>
       <Text style={[styles.fontNunitoRegular, styles.fontR, styles.textMuted]}>{placeholder}</Text>
@@ -94,6 +159,7 @@ export function TextInput(props: TextInputProps) {
 
 export function TouchableOpacity(props: TouchableOpacityProps) {
   const { btnText, style, ...otherProps } = props
+  const styles = useGlobalStyles()
 
   return (
     <DefaultTouchableOpacity style={[styles.btn1, style]} {...otherProps}>
@@ -104,9 +170,10 @@ export function TouchableOpacity(props: TouchableOpacityProps) {
 
 export function TextButton(props: TouchableOpacityProps) {
   const { btnText, style, ...otherProps } = props
+  const styles = useGlobalStyles()
 
   return (
-    <DefaultTouchableOpacity style={[{backgroundColor:'transparent'}, style]} {...otherProps}>
+    <DefaultTouchableOpacity style={[{ backgroundColor: 'transparent' }, style]} {...otherProps}>
       <Text style={[styles.fontNunitoBold, styles.fontM, styles.btnTextLink]}>{btnText}</Text>
     </DefaultTouchableOpacity>
   )
