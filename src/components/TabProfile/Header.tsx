47108eebf7787cb70a8774d6b1f5c673e01b79ab
tadeumelembe <tadeumelembe@gmail.com>
2023-05-03 03:37:26 +0200

    Added profile header

--- src/components/TabProfile/Header.tsx
+++ src/components/TabProfile/Header.tsx
@@ -1,30 +1,72 @@
 import React from "react"
 import { StyleSheet, TouchableOpacity } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Octicons } from '@expo/vector-icons';
 
-import { View, Text } from "../../../components/Themed"
+import { View, Text, Avatar } from "../../../components/Themed"
 import style from "../../../constants/style";
 import Colors from "../../../constants/Colors";
 
-const Header: React.FC = () => {
+const HeaderProfile: React.FC = () => {
     return (
         <View style={localStyle.root}>
-            <Text style={localStyle.title}>My Kitchen</Text>
-            <TouchableOpacity style={localStyle.settingSection}>
-                <Ionicons name="settings-outline" size={22} color="black" />
-                <Text style={localStyle.settingsText}>Settings</Text>
-            </TouchableOpacity>
+
+            <View style={localStyle.rowView}>
+                <Text style={localStyle.title}>My Kitchen</Text>
+                
+                <TouchableOpacity style={localStyle.settingSection}>
+                    <Ionicons name="settings-outline" size={22} color="black" />
+                    <Text style={localStyle.settingsText}>Settings</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={[localStyle.rowView, { marginTop: 30 }]}>
+                <Avatar size="xl" />
+
+                <View style={[localStyle.column, { marginLeft: 15 }]}>
+                    <View style={localStyle.column}>
+                            <Text style={localStyle.profileName}>Nick Evans</Text>
+                        <Text style={localStyle.secondaryText}>Nick Evans</Text>
+                    </View>
+
+                    <View style={[localStyle.row,{alignItems:'center'}]}>
+                        <Text style={localStyle.secondaryText}>584 followers</Text>
+                        <View style={{ marginHorizontal: 8 }}>
+                            <Octicons name="dot-fill" size={15} color="black" />
+                        </View>
+                        <Text style={localStyle.secondaryText}>23k likes</Text>
+                    </View>
+                </View>
+            </View>
+            
         </View>
     )
 }
 
 const localStyle = StyleSheet.create({
     root: {
+        flexDirection: 'column',
+        flex: 1,
+        width: '100%',
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1
+    },
+    column: {
+        flexDirection: 'column',
+        flex: 1
+    },
+    rowView: {
         ...style.row,
+        flex: 0,
+        width: '100%',
+        alignItems: 'center',
         justifyContent: 'space-between'
     },
     title: {
         ...style.fontXl,
+        flex: 0,
+
         ...style.fontNunitoBold,
     },
     settingSection: {
@@ -38,7 +80,16 @@ const localStyle = StyleSheet.create({
         ...style.fontNunitoBold,
         color: Colors.light.tint,
         paddingLeft: 5
+    },
+    profileName: {
+        ...style.fontM,
+        ...style.fontNunitoBold
+    },
+    secondaryText: {
+        ...style.fontR,
+        ...style.textMuted2,
+        ...style.fontNunitoRegular
     }
 })
 
-export default Header
+export default HeaderProfile
