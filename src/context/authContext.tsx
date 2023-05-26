43dbacbf351ed4d68367b5bf571e18389a3b3d04
tadeumelembe <tadeumelembe@gmail.com>
2023-05-26 02:24:20 +0200

    Added sign in, sign out and restore

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,35 +1,67 @@
-import React, { ChildContextProvider, Children, createContext, useContext, useState } from "react";
+import React, { ChildContextProvider, Children, createContext, useContext, useEffect, useMemo, useState } from "react";
 
 import { IUserContext } from '../components/types'
+import { auth, onAuthStateChanged } from "../../firebaseConfig";
+import { signOut as firebaseSignOut } from "firebase/auth"
 
 interface IAuthContext {
     isSigned: boolean;
-    signIn(): void;
+    signIn(user: object): void;
     signOut(): void;
     user: IUserContext | null;
+    loading: boolean
 }
 
 const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 
 export const AuthProvider: React.FC<Element> = ({ children }) => {
     const [user, setUser] = useState<IUserContext | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
 
-    async function signIn() {
+    useEffect(() => {
+        restoreState()
+    }, [])
+
+    function signIn(user: object) {
         setUser({
-            id: '1',
-            name: 'Tadeu Melembe',
-            email: 'tadeumelembe@gmail.com'
+            id: user.uuid,
+            name: user.displayName,
+            email: user.email
         })
     }
 
     async function signOut() {
-        setUser(null)
+        firebaseSignOut(auth).then(() => {
+            setUser(null)
+        }).catch((error) => {
+            alert('Somethingwent wrong. Please, try again')
+        });
     }
+
+    function restoreState() {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                signIn(user)
+            }
+            setLoading(false)
+
+        });
+    }
+
+    const memorizedValues = useMemo(
+        () => ({
+            user,
+            isSigned: !!user,
+            signOut,
+            signIn,
+            loading
+        }), [user, loading]
+    )
 
 
     //return {user};
     return (
-        <AuthContext.Provider value={{ user, isSigned: !!user, signOut, signIn, }}>
+        <AuthContext.Provider value={memorizedValues}>
             {children}
         </AuthContext.Provider>
     );
